728666a2b2ce0f8b20640cdbbda5678182f9f38e
akashahmad <engr.akashahmad@gmail.com>
2020-04-07 23:23:56 -0700

    logo changed!!!

---
+++ src/components/fantacy-card/index.js
@@ -0,0 +1,57 @@
+import React from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
+export default () => {
+  AOS.init()
+  return (
+    <section className="fantacy">
+      <div className="fantacy-container">
+        <div
+          className="text-container"
+          data-aos="fade-right"
+          data-aos-delay="50"
+          data-aos-duration="2000"
+        >
+          <div>
+            <h2>We Are Fantasy We've got you covered on</h2>
+          </div>
+        </div>
+        <div
+          className="card-main"
+          data-aos="fade-left"
+          data-aos-delay="50"
+          data-aos-duration="2000"
+        >
+          <div className="card-container">
+            <div className="head-img">
+              <img src={require("../../images/product design.png")} alt="" />
+            </div>
+            <div className="card-content para-font-size">
+              <h2>Product Design</h2>
+              <p>
+                Every product must make a statement and we’re ready to create it
+                with holistic approach to design.
+              </p>
+              <p>
+                We translate your business goals and objectives into insightful
+                UX, remarkable UI and distinctive
+              </p>
+            </div>
+          </div>
+          <div className="card-container">
+            <div className="head-img">
+              <img src={require("../../images/moonpng.png")} alt="" />
+            </div>
+            <div className="card-content para-font-size">
+              <h2>Creative Web Solution</h2>
+              <p>
+                We translate your business goals and objectives into insightful
+                UX, remarkable UI and distinctive{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,11 +1,13 @@
 import React, { ReactElement, useState, useEffect } from "react"
 import Header from "../components/services-header/index"
 import Footer from "../components/footer/index"
+import Content from "../components/services-content/index"
 
 export default () => {
   return (
     <div>
       <Header />
+      <Content />
       <Footer />
     </div>
   )
