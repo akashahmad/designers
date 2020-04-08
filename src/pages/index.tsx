f7a4e3371634cac246eca14a169a51d9fb19da8e
akashahmad <engr.akashahmad@gmail.com>
2020-04-08 07:22:12 +0500

    title and favicon added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Fantacy from "../components/fantacy-card/index"
 import Whowe from "../components/who-we-are/index"
 import Services from "../components/services/index"
 import Agency from "../components/agency-task/index"
+import { Helmet } from "react-helmet"
 
 interface Props {}
 
@@ -12,6 +13,10 @@ function Index({}: Props): ReactElement {
   const [isMenuOpen, setMenuOpen] = useState(false)
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Geeks Hub</title>
+      </Helmet>
       <div>
         <Header />
         <Fantacy />
