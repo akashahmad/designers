a56362d25ac55726fd24690b43aad8e1a63f2ce6
akashahmad <engr.akashahmad@gmail.com>
2020-04-15 08:48:13 +0500

    rebuild

--- src/pages/our-services.tsx
+++ src/pages/our-services.tsx
@@ -14,13 +14,13 @@ export default () => {
                 <meta property="og:locale" content="en_US"/>
                 <meta property="og:type" content="website"/>
                 <meta name="description"
-                      content="We build dev teams. On-demand business web and application development services that balance code, cost, and compliance. Premium quality. Ethical sourcing."/>
+                      content="We build business web and application development services that balance code, cost, and compliance. Premium quality. Ethical sourcing."/>
                 <meta name="keywords"
                       content="Development, Mobile Development, UX/UI, NodeJS, javascript, web design, web dev"/>
                 <meta name="author" content="GeeksHub"/>
-                <meta property="og:title" content="We build dev teams. Sustainable app and web development services."/>
+                <meta property="og:title" content="We build sustainable app and web development services."/>
                 <meta property="og:description"
-                      content="We build dev teams. On-demand business web and application development services that balance code, cost, and compliance. Premium quality. Ethical sourcing."/>
+                      content="We build business web and application development services that balance code, cost, and compliance. Premium quality. Ethical sourcing."/>
                 <meta property="og:url" content="https://www.geekshub.org/"/>
                 <meta property="og:site_name" content="GeeksHub"/>
                 <meta property="og:image" content="/static/images/Black.png"/>
@@ -28,8 +28,8 @@ export default () => {
                 <meta property="og:image:height" content="420"/>
                 <meta name="twitter:card" content="summary"/>
                 <meta name="twitter:description"
-                      content="We build dev teams. On-demand business web and application development services that balance code, cost, and compliance. Premium quality. Ethical sourcing."/>
-                <meta name="twitter:title" content="We build dev teams. Sustainable app and web development services."/>
+                      content="We build business web and application development services that balance code, cost, and compliance. Premium quality. Ethical sourcing."/>
+                <meta name="twitter:title" content="We build sustainable app and web development services."/>
                 <meta name="twitter:site" content="@Geeks__Hub"/>
                 <meta name="twitter:image" content="/static/images/Black.png"/>
                 <meta name="twitter:creator" content="@Geeks__Hub"/>
