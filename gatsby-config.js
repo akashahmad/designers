9f4317d8a4c58e83a4758ac890e5e81cfb594fa3
akashahmad <engr.akashahmad@gmail.com>
2020-04-15 06:57:14 +0500

    robots text file plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-sitemap`,
+        'gatsby-plugin-robots-txt',
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
