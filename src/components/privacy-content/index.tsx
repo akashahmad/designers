0f42630dc68f2308c2471573a2b5cafe7525ad66
akashahmad <engr.akashahmad@gmail.com>
2020-04-18 00:31:11 -0700

    privacy page added

---
+++ src/components/privacy-content/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from "gatsby"
+
+export default () => {
+    return(
+        <div className="fourofour-page privacy-page">
+            <div>
+                <div className="content-section">
+                    <h2>What Are Cookies</h2>
+                    <p>Per common practice by almost all modern professional websites; this site uses cookies. Cookies are tiny files that are downloaded to a user’s computer, to improve their overall browsing experience. This page describes what information they gather, how we use it and why we sometimes need to store cookies. We will also share how you can prevent cookies from being stored. You should know that doing so may downgrade or ‘break’ certain elements of this site’s functionality.</p>
+                    <h2>What Are Cookies</h2>
+                    <p>Per common practice by almost all modern professional websites; this site uses cookies. Cookies are tiny files that are downloaded to a user’s computer, to improve their overall browsing experience. This page describes what information they gather, how we use it and why we sometimes need to store cookies. We will also share how you can prevent cookies from being stored. You should know that doing so may downgrade or ‘break’ certain elements of this site’s functionality.</p>
+                    <ul>
+                        <li>demo</li>
+                        <li>demo</li>
+                        <li>demo</li>
+                    </ul>
+                    <h2>More Information</h2>
+                    <p>If cookies are something that you’re unsure you need, it’s usually safer to leave them enabled, anyway. Just in case it does interact with one of the features you use on our site. However, if you’re still looking for more information we invite you to contact us through this website to learn more.</p>
+                    <p>If cookies are something that you’re unsure you need Just in case it does interact with one of the features you use on our site. However, if you’re still looking for more information we invite you to contact us through this website to learn more.</p>
+                    <p>Regards and thanks,</p>
+                    <Link to="/">admin@geekshub.com</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
