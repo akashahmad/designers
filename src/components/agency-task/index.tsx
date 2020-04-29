e686542ad72e5e77155d009d98fb658b794e54da
akashahmad <engr.akashahmad@gmail.com>
2020-04-28 19:22:15 -0700

    contact updated

--- src/components/agency-task/index.tsx
+++ src/components/agency-task/index.tsx
@@ -14,6 +14,23 @@ export default () => {
               <h2>We have done lot of Agency Task</h2>
             </div>
           </div>
+          <div className="agency-carousel">
+          {/* <Carousel> */}
+            <div className="agency-popup"> 
+                {/* <div className="agency-content">
+                  <div>
+                    <div className="image-popup"></div>
+                  </div>
+                  <div>
+                     <div className="content-info">
+                        <h2>Website Title</h2>
+                        <span>Website Link</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
+                     </div>
+                  </div>
+                </div> */}
+            </div>
           <div className="task-container-primary">
             <div>
               <div className="task-container image-task-one">
@@ -87,6 +104,9 @@ export default () => {
                 </div>
               </div>
             </div>
+          </div>
+          {/* <div>2</div> */}
+          {/* </Carousel> */}
           </div>
           <div className="task-slider">
             <Carousel showThumbs={false}>
