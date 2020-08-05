36202771dfa748dd2abde44c7dc1e888660648e1
akashahmad <engr.akashahmad@gmail.com>
2020-08-06 01:18:49 +0500

    Shehnam picture added

--- src/components/our-team/index.tsx
+++ src/components/our-team/index.tsx
@@ -43,6 +43,30 @@ export default () => {
                                 ></div>
                                 <div className="name-profile">
                                     <h3>Muqarrab Khan</h3>
+                                    <p>Co-Founder and Product Designer</p>
+                                </div>
+                                {/* <div className="social-link">
+                                    <div className="social-container">
+                                        <a href="/#">
+                                            <i className="fa fa-facebook"></i>
+                                        </a>
+                                    </div>
+                                    <div className="social-container">
+                                        <a href="/#">
+                                            <i className="fa fa-facebook"></i>
+                                        </a>
+                                    </div>
+                                </div> */}
+                            </div>
+                        </div>
+                        <div className="slider-settings">
+                            <div className="team-primary">
+                                <div
+                                    className="team-container-img profile-three"
+
+                                ></div>
+                                <div className="name-profile">
+                                    <h3>Shehnam Zubair</h3>
                                     <p>Co-Founder and Lead UI/UX Designer</p>
                                 </div>
                                 {/* <div className="social-link">
@@ -94,6 +118,30 @@ export default () => {
                             ></div>
                             <div className="name-profile">
                                 <h3>Muqarrab Khan</h3>
+                                <p>Co-Founder and Product Designer</p>
+                            </div>
+                            {/* <div className="social-link">
+                                <div className="social-container">
+                                    <a href="/#">
+                                        <i className="fa fa-facebook"></i>
+                                    </a>
+                                </div>
+                                <div className="social-container">
+                                    <a href="/#">
+                                        <i className="fa fa-facebook"></i>
+                                    </a>
+                                </div>
+                            </div> */}
+                        </div>
+                    </div>
+                    <div>
+                        <div className="team-primary">
+                            <div
+                                className="team-container-img profile-three"
+                                data-aos="flip-left"
+                            ></div>
+                            <div className="name-profile">
+                                <h3>Shehnam Zubair</h3>
                                 <p>Co-Founder and Lead UI/UX Designer</p>
                             </div>
                             {/* <div className="social-link">
