daa117c9770a5cdc18c8c0d06a5a4824bbae1809
afaq-geekshub <afaq.geekshub@gmail.com>
2020-09-11 16:41:19 +0500

    Added color change on hover to nav items and formatted the phone number

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -23,13 +23,13 @@ export default () => {
                 </div>
                 <div>
                   <ul>
-                    <li className="nav-hover">
+                    <li >
                       <Link to="/who-we-are" className="nav-hover">Who We Are</Link>
                     </li>
-                    <li className="nav-hover">
+                    <li >
                       <Link to="/our-services">Our Services</Link>
                     </li>
-                    <li className="nav-hover">
+                    <li >
                       <Link to="/contact-us">Contact Us</Link>
                     </li>
                   </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  purge: [],
   theme: {
-    extend: {
-      colors:{
-        'white-100':"#F7F7F7",
-        'white-200':'#DDDDDD'
-      }
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
